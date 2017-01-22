3a09aeb3e8bc6d106656a36c063e6296de0f70c7
bojonghome <benojong@hotmail.com>
2017-01-22 21:17:53 +0000

    Initial creation

---
+++ VS Demo/VS Demo/NewClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VS_2010_Demo
+{
+    class NewClass
+    {
+        //Additional 1.1 changes
+    }
+}
---
+++ VS Demo/VS Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VS_2010_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Changes on the Main Branch from VS 2010
+            //v1.1 changes
+
+            //master changes
+            //these changes will be stashed
+
+            //Change from Visual Studio 2012
+
+            //1.1.5 changes
+
+            //some changes
+        }
+    }
+}
